8ff32886973b66ccdc05692b7011e9164e4454b8
simonjujuh <juhel.simon6@gmail.com>
2020-04-11 17:23:34 +0200

    Add installation script

--- install.sh
+++ install.sh
@@ -5,3 +5,17 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # git
 ln -sf ${BASEDIR}/gitconfig ~/.gitconfig
 ln -sf ${BASEDIR}/git-completion.bash ~/.git-completion.bash
+
+# tmux
+ln -sf ${BASEDIR}/tmux.conf ~/.tmux.conf
+
+# vim
+ln -sf ${BASEDIR}/vimrc ~/.vimrc
+ln -sf ${BASEDIR}/vim/ ~/.vim
+
+# cheat
+ln -sf ${BASEDIR}/cheat-completion.bash ~/.cheat-completion.bash
+ln -sf ${BASEDIR}/cheat ~/.cheat
+
+# bashrc
+ln -sf ${BASEDIR}/bashrc ~/.bashrc
