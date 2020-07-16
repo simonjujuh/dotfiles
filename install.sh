0b7794ae6adb458ec175ad7a65e8dbd7b573d3ee
simonjujuh <juhel.simon6@gmail.com>
2020-07-16 18:48:52 +0200

    Remove unused links

--- install.sh
+++ install.sh
@@ -11,14 +11,11 @@ ln -sf ${BASEDIR}/tmux.conf ~/.tmux.conf
 
 # vim
 ln -sf ${BASEDIR}/vimrc ~/.vimrc
-# ln -sf ${BASEDIR}/vim/ ~/.vim
+
+# bashrc
+ln -sf ${BASEDIR}/bashrc ~/.bashrc
 
 # cheat
 # ln -sf ${BASEDIR}/cheat-completion.bash ~/.cheat-completion.bash
 # ln -sf ${BASEDIR}/cheat ~/.cheat
 
-# bashrc
-ln -sf ${BASEDIR}/bashrc ~/.bashrc
-
-# xinitrc
-ln -sf ${BASEDIR}/xinitrc ~/.xinitrc
