cf273d5cfaaa804a4d9236d07edaa8ac7f2a8c78
simonjujuh <juhel.simon6@gmail.com>
2020-11-27 16:43:07 +0100

    Update i3 configuration and install.sh accordingly

--- install.sh
+++ install.sh
@@ -17,8 +17,19 @@ ln -sf ${BASEDIR}/bashrc ~/.bashrc
 
 # cheat
 ln -sf ${BASEDIR}/cheat.bash ~/.cheat.bash
+ln -sf ${BASEDIR}/cheat.zsh ~/.cheat.zsh
 
 # i3 setup
 ln -sf ${BASEDIR}/i3/config ~/.config/i3/config
+ln -sf ${BASEDIR}/i3/scripts ~/.config/i3/scripts
+ln -sf ${BASEDIR}/i3/blocks ~/.config/i3/blocks
+ln -sf ${BASEDIR}/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -sf ${BASEDIR}/rofi/config ~/.config/rofi/config
-ln -sf ${BASEDIR}/compton.conf ~/.config/compton.conf
+ln -sf ${BASEDIR}/compton/compton.conf ~/.config/compton.conf
+
+sudo cp ${BASEDIR}/i3/scripts/battery-monitor.service /etc/systemd/system/battery-monitor.service
+sudo systemctl enable battery-monitor
+sudo systemctl start battery-monitor
+sudo cp ${BASEDIR}/i3/scripts/battery-monitor.timer /etc/systemd/system/battery-monitor.timer
+sudo systemctl enable battery-monitor.timer
+sudo systemctl start battery-monitor.timer
