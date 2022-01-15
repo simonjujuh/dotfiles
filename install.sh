1b71040eee36e5f297957a3d6dd4f43b4f5f1342
simonjujuh <juhel.simon6@gmail.com>
2022-01-15 10:51:07 +0100

    Update content: Sat Jan 15 10:50:32 AM CET 2022

--- install.sh
+++ install.sh
@@ -15,3 +15,11 @@ ln -sf ${BASEDIR}/vimrc ~/.vimrc
 ln -sf ${BASEDIR}/bashrc ~/.bashrc
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
 
+# misc
+echo "[*] Installation done"
+echo "    Don't forget to install the following dependencies"
+echo "    - https://ohmyz.sh/"
+echo "    - https://github.com/tmux-plugins/tpm"
+echo "    - https://github.com/tmux-plugins/tmux-logging"
+echo "    - https://github.com/VundleVim/Vundle.vim (and call PluginInstall)"
+echo ""
